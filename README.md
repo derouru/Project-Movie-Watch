98fccde3eee329138a6ecce4f965f2e933f8c1ee
gabrielcalubayan72 <90253997+gabrielcalubayan72@users.noreply.github.com>
2025-02-17 20:45:07 +0800

    create and index php

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Project-Movie-Watch
+
+Place the 'mymovies' folder in C:/xampp/htdocs/
---
+++ mymovies/create.php
@@ -0,0 +1,43 @@
+<?php
+$name = "";
+$watched = "";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Movies</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container my-5">
+        <h2>New Movie</h2>
+        <form method="post">
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label">Name</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="name" value="<?php echo $name; ?>">
+                </div>
+            </div>
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label">When watched?</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="watched" value="<?php echo $watched; ?>">
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <div class="offset-sm-3 col-sm-3 d-grid">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+                <div class="col-sm-3 d-grid">
+                    <a class="btn btn-outline-primary" href="/myshop/index.php" role="button">Cancel</a>
+                </div>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
