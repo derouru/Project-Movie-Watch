bfb735c6bc21f16a30a467b1f51cf8c2adeed079
gabrielcalubayan72 <90253997+gabrielcalubayan72@users.noreply.github.com>
2025-02-17 21:08:10 +0800

    feat: adding movies

--- mymovies/create.php
+++ mymovies/create.php
@@ -1,6 +1,53 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "mymovies";
+
+// creating connection
+$connection = new mysqli($servername, $username, $password, $database);
+
 $name = "";
 $watched = "";
+
+$errorMessage = "";
+$successMessage = "";
+
+// submitting POST requests
+if ( $_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = $_POST["name"];
+    $watched = $_POST["watched"];
+
+    // checking if fields are complete 
+    do {
+        if ( empty($name) || empty($watched) ) {
+            $errorMessage = "All the fields are required.";
+            break;
+        }
+        
+        // add new movie to database
+        $sql = "INSERT INTO movies (name, watched) " .
+                "VALUES ('$name', '$watched')";
+        $result = $connection->query($sql);
+
+        // check if query is successful
+        if (!$result) {
+            $errorMessage = "Invalid query: " . $connection->error;
+            break;
+        }
+
+        $name = "";
+        $watched = "";
+
+        $successMessage = "Movie added correctly.";
+        
+        // redirecting user to index file (list of movies), and exit execution of this file
+        header("location: /mymovies/index.php");
+        exit;
+
+    } while (false);
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -11,10 +58,23 @@ $watched = "";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Movies</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
     <div class="container my-5">
         <h2>New Movie</h2>
+
+        <?php
+        if ( !empty($errorMessage) ) {
+            echo "
+                <div class='alert alert-warning alert-dismissible fade show' role='alert'>
+                    <strong>$errorMessage</strong>
+                    <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+                </div>
+            ";
+        }
+        ?>
+
         <form method="post">
             <div class="row mb-3">
                 <label class="col-sm-3 col-form-label">Name</label>
@@ -28,6 +88,21 @@ $watched = "";
                     <input type="text" class="form-control" name="watched" value="<?php echo $watched; ?>">
                 </div>
             </div>
+
+            <?php
+            if ( !empty($successMessage) ) {
+                echo "
+                <div class='row mb-3'>
+                    <div class='offset-sm-3 col-sm-6'>
+                        <div class='alert alert-success alert-dismissible fade show' role='alert'>
+                            <strong>$successMessage</strong>
+                            <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+                        </div>
+                    </div>                
+                </div>                    
+                ";
+            }
+            ?>
 
             <div class="row mb-3">
                 <div class="offset-sm-3 col-sm-3 d-grid">
